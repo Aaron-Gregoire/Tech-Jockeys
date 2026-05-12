a9fb8342c8ad5c9267577f0a0474486e9bbf2c7b
Aaron-Gregoire <aaronjemmett@gmail.com>
2026-05-12 15:23:02 -0400

    created 3 controllers and made store views

--- tech-jockeys-master/TechJockeys/Controllers/StoreController.cs
+++ tech-jockeys-master/TechJockeys/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechJockeys.Models;
 
 namespace TechJockeys.Controllers
 {
@@ -6,6 +7,31 @@ namespace TechJockeys.Controllers
     {
         public IActionResult Index()
         {
+            // fetch category data ( mock today, from db later)
+            var categories = new List<Category>();
+
+            // create mock category list
+            for (int i =1; i< 16; i++)
+            {
+                categories.Add(new Category { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            //load view and pass category list
+            return View(categories);
+        }
+
+        public IActionResult ByCategory(int id)
+        {
+            // error handle if id missing => redirect to Store index so user can choose a category
+            if (id == 0 || id > 15)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // use id param to find category 
+            // use ViewData dictionary to show selcted category names in heading 
+            ViewData["Category"] = "Category " + id.ToString();
+
             return View();
         }
     }
---
+++ tech-jockeys-master/TechJockeys/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace TechJockeys.Models
+{
+    public class Category
+    {
+        // using {ModelName}Id as the property tells the server automatically this is a PK
+        public int CategoryId { get; set; }
+
+        public String Name { get; set; }
+
+
+
+
+    }
+}
