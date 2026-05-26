d67c7bf14b5795c5b7939a4dc424d4cdb4c51080
Aaron-Gregoire <aaronjemmett@gmail.com>
2026-05-26 14:39:16 -0400

    fetch category list

--- tech-jockeys-master/TechJockeys/Controllers/CategoriesController.cs
+++ tech-jockeys-master/TechJockeys/Controllers/CategoriesController.cs
@@ -1,22 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechJockeys.Data;
 using TechJockeys.Models;
 
 namespace TechJockeys.Controllers
 {
     public class CategoriesController : Controller
     {
+        // shared db conn available to all controller method 
+        private readonly ApplicationDbContext _context;
+
+        // constructor with db dependancy 
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            // create empty category list so view doesnt crash
-            var categories = new List<Category>();
+            // fetch category list from db using dbset object 
+            var categories = _context.Category.ToList();
 
             // pass empty list to view
             return View(categories);
         }
 
+        // GET: /Categories/Create => show empty Category form
         public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: /Categories/Create => process form submission and create new category
+        [HttpPost]
+        public IActionResult Create([Bind("Name")] Category category)
+        {
+            // validate input. show form again if invaid
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // create new category and save to db
+            _context.Category.Add(category);
+            _context.SaveChanges();
+
+            // redirect to index to see updated list of catgories
+            return RedirectToAction("Index");
         }
 
         public IActionResult Edit()
